f3374c0f77caee6250121883f20c2e40af057847
Maikel Ordaz <maikelordaz@gmail.com>
2024-09-04 23:28:23 -0400

    deployment

---
+++ script/DeployTokenBlacklist.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {TokenBlacklist} from "src/TokenBlacklist.sol";
+
+pragma solidity 0.8.26;
+
+contract DeployTokenBlacklist is Script {
+    function run() public returns (TokenBlacklist) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        HelperConfig.NetworkConfig memory config = helperConfig
+            .getConfigByChainId(block.chainid);
+
+        vm.startBroadcast();
+        TokenBlacklist token = new TokenBlacklist(config.tokenAdmin);
+        vm.stopBroadcast();
+
+        return token;
+    }
+}
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.26;
+
+import {Script, console2} from "forge-std/Script.sol";
+
+abstract contract CodeConstants {
+    /*//////////////////////////////////////////////////////////////
+                               CHAIN IDS
+    //////////////////////////////////////////////////////////////*/
+    uint256 public constant ARB_SEPOLIA_CHAIN_ID = 421614;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+
+    /*//////////////////////////////////////////////////////////////
+                               ACCOUNTS
+    //////////////////////////////////////////////////////////////*/
+
+    struct TokenAdmin {
+        address local;
+        address sepolia;
+    }
+
+    TokenAdmin public tokenAdmin =
+        TokenAdmin({
+            local: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, // Anvil's account 0
+            sepolia: 0x3904F59DF9199e0d6dC3800af9f6794c9D037eb1
+        });
+}
+
+contract HelperConfig is CodeConstants, Script {
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error HelperConfig__InvalidChainId();
+
+    /*//////////////////////////////////////////////////////////////
+                                 TYPES
+    //////////////////////////////////////////////////////////////*/
+
+    struct NetworkConfig {
+        address tokenAdmin;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    // Local network state variables
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    constructor() {
+        networkConfigs[ARB_SEPOLIA_CHAIN_ID] = getArbSepoliaConfig();
+    }
+
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
+        if (chainId == ARB_SEPOLIA_CHAIN_ID) {
+            return networkConfigs[chainId];
+        } else if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilConfig();
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
+    }
+
+    function getArbSepoliaConfig()
+        public
+        view
+        returns (NetworkConfig memory sepoliaNetworkConfig)
+    {
+        sepoliaNetworkConfig = NetworkConfig({tokenAdmin: tokenAdmin.sepolia});
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              LOCAL CONFIG
+    //////////////////////////////////////////////////////////////*/
+
+    function getOrCreateAnvilConfig() public returns (NetworkConfig memory) {
+        return NetworkConfig({tokenAdmin: tokenAdmin.local});
+    }
+}

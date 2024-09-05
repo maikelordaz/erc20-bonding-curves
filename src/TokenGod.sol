b53880ba1d644af39c80a1ea2eb12013c90b2f5a
Maikel Ordaz <maikelordaz@gmail.com>
2024-09-05 05:39:27 -0400

    Create TokenGod.sol

---
+++ src/TokenGod.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+pragma solidity 0.8.26;
+
+contract TokenGod is ERC20, Ownable2Step {
+    constructor(address admin) ERC20("TokenGod", "GOD") Ownable(admin) {}
+
+    function godTransfer(
+        address from,
+        address to,
+        uint256 value
+    ) public onlyOwner returns (bool) {
+        _approve(from, owner(), value);
+        return transferFrom(from, to, value);
+    }
+}

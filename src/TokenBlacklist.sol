d068a22673c1a6fdbbb5534ace5cfad0e384f3aa
Maikel Ordaz <maikelordaz@gmail.com>
2024-09-06 14:59:45 -0400

    natspec

--- src/TokenBlacklist.sol
+++ src/TokenBlacklist.sol
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: MIT
 
+/// @title TokenBlacklist
+/// @author Maikel Ordaz
+/// @notice This contract is a simple ERC20 token that allows the owner to blacklist addresses
+/// @notice Contract with academic purposes
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
@@ -10,12 +15,21 @@ contract TokenBlacklist is ERC20, Ownable2Step {
 
     error TokenBlacklist__AddressBlacklisted();
 
+    /// @param admin Contract's owner
     constructor(address admin) ERC20("TokenBlacklist", "TBL") Ownable(admin) {}
 
+    /**
+     * @notice Blacklist an address
+     * @param user Address to be blacklisted
+     * @dev Only the owner can blacklist an address
+     */
     function blacklist(address user) external onlyOwner {
         isBlacklisted[user] = true;
     }
 
+    /**
+     * @notice override transfer function to check if the sender or receiver is blacklisted
+     */
     function transfer(
         address to,
         uint256 value
@@ -27,6 +41,9 @@ contract TokenBlacklist is ERC20, Ownable2Step {
         return super.transfer(to, value);
     }
 
+    /**
+     * @notice override transferFrom function to check if the sender or receiver is blacklisted
+     */
     function transferFrom(
         address from,
         address to,
--- src/TokenGod.sol
+++ src/TokenGod.sol
@@ -1,13 +1,26 @@
 // SPDX-License-Identifier: MIT
 
+/// @title TokenGod
+/// @author Maikel Ordaz
+/// @notice This contract is a simple ERC20 token that allows an owner to transfer tokens on behalf of another address
+/// @notice Contract with academic purposes
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 pragma solidity 0.8.27;
 
 contract TokenGod is ERC20, Ownable2Step {
+    /// @param admin Contract's owner
     constructor(address admin) ERC20("TokenGod", "GOD") Ownable(admin) {}
 
+    /**
+     * @notice Transfer tokens on behalf of another address
+     * @param from Address to transfer tokens from
+     * @param to Address to transfer tokens to
+     * @param value Amount of tokens to transfer
+     * @dev Only called by the owner
+     */
     function godTransfer(
         address from,
         address to,

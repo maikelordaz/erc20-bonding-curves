7e07e902fbbaa36080011404f09aac64fac5b752
Maikel Ordaz <maikelordaz@gmail.com>
2024-09-06 22:12:14 -0400

    deploy

--- script/DeployBondingCurve.s.sol
+++ script/DeployBondingCurve.s.sol
@@ -2,13 +2,19 @@
 
 import {Script} from "forge-std/Script.sol";
 import {BondingCurve} from "src/BondingCurve.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 pragma solidity 0.8.27;
 
 contract DeployBondingCurve is Script {
     function run() public returns (BondingCurve) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        HelperConfig.NetworkConfig memory config = helperConfig
+            .getConfigByChainId(block.chainid);
+
         vm.startBroadcast();
-        BondingCurve curve = new BondingCurve();
+        BondingCurve curve = new BondingCurve(config.tokenAdmin);
         vm.stopBroadcast();
 
         return curve;
